b5dfca193be1f425f7baf7ebcd46679ec3f9707e
pmayer <pinhas_mayer@intuit.com>
2023-08-09 17:10:12 +0300

    updated unit test

--- app/test_app.py
+++ app/test_app.py
@@ -9,17 +9,24 @@ class CounterServiceTestCase(unittest.TestCase):
     def test_get_request(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Counter:', response.data)
+        self.assertEqual(response.get_json(), {"counter": 0})
 
     def test_post_request(self):
         response = self.client.post('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'counter incremented', response.data)
+        self.assertEqual(response.get_json(), {"message": "Counter incremented successfully"})
 
     def test_counter_increment(self):
         for i in range(5):
             response = self.client.post('/')
-            self.assertIn(f'Counter: {i + 1}'.encode(), response.data)
+            self.assertEqual(response.get_json(), {"message": "Counter incremented successfully"})
+            get_response = self.client.get('/')
+            self.assertEqual(get_response.status_code, 200)
+            self.assertEqual(get_response.json(), {"counter": i + 1})
+        
+    def test_unsupported_put_request(self):
+        response = self.client.put('/')
+        self.assertEqual(response.status_code, 405) # 405 Method Not Allowed
 
 if __name__ == '__main__':
     unittest.main()
