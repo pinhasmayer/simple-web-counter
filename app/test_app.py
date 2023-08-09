ddb1902a9ddf7c130ee59d35ce2dc573c7cc3abc
pmayer <pinhas_mayer@intuit.com>
2023-08-09 14:30:09 +0300

    added simple test

---
+++ app/test_app.py
@@ -0,0 +1,26 @@
+import unittest
+from app import app
+
+class CounterServiceTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.client = app.test_client()
+
+    def test_get_request(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Counter:', response.data)
+
+    def test_post_request(self):
+        response = self.client.post('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Counter: 1', response.data)
+
+    def test_counter_increment(self):
+        for i in range(5):
+            response = self.client.post('/')
+            self.assertIn(f'Counter: {i + 1}'.encode(), response.data)
+
+if __name__ == '__main__':
+    unittest.main()
+
